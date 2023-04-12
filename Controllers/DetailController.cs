ab17b31a3bf4d145405956bfd40549060a306fa1
tranductri2003 <89126960+tranductri2003@users.noreply.github.com>
2023-04-12 08:31:56 +0700

    fixing delete

--- Controllers/DetailController.cs
+++ Controllers/DetailController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.CodeAnalysis;
+using Microsoft.EntityFrameworkCore;
 using StudentManagementDemo.Models.Entities;
 
 namespace StudentManagementDemo.Controllers
@@ -30,6 +32,53 @@ namespace StudentManagementDemo.Controllers
             var s = db.Students.Where(p => p.StudentId == StudentId).Select(p => p).ToList();
             return View(s);
             
+        }
+        public IActionResult Delete(string StudentId)
+        {
+            //using (var context = new StudentManagementDemoContext())
+            //{
+            //    // Tìm sinh viên cần xóa
+            //    var student = context.Students.Find(StudentId);
+
+            //    // Xóa các tài khoản liên quan đến sinh viên
+            //    foreach (var account in student.Accounts.ToList())
+            //    {
+            //        context.Accounts.Remove(account);
+            //    }
+
+            //    // Xóa sinh viên
+            //    context.Students.Remove(student);
+
+            //    // Lưu thay đổi vào cơ sở dữ liệu
+            //    context.SaveChanges();
+            //}
+            return RedirectToAction("LoginSuccess", "Detail", new { StudentID = "Admin" });
+        }
+        public IActionResult Save(string StudentID, string Name, string Address, string DateOfBirth, bool Gender, double Score)
+        {
+
+            //System.Diagnostics.Debug.WriteLine(StudentID+Name+ Address);
+            StudentManagementDemoContext db = new StudentManagementDemoContext();
+            using (db)
+            {
+                // Lấy đối tượng bản ghi cần cập nhật từ cơ sở dữ liệu
+                var student = db.Students.FirstOrDefault(s => s.StudentId == StudentID);
+
+                if (student != null)
+                {
+                    // Cập nhật giá trị của trường trong đối tượng bản ghi
+                    student.StudentId = StudentID;
+                    student.Name = Name;
+                    student.Address = Address;
+                    student.DateOfBirth = DateOfBirth;
+                    student.Gender = Gender;
+                    student.Score= Score;
+
+                    // Lưu các thay đổi vào cơ sở dữ liệu
+                    db.SaveChanges();
+                }
+            }
+            return RedirectToAction("LoginSuccess", "Detail", new { StudentID = "Admin" });
         }
     }
 }
--- Models/Entities/Student.cs
+++ Models/Entities/Student.cs
@@ -16,4 +16,5 @@ public partial class Student
     public bool? Gender { get; set; }
 
     public double? Score { get; set; }
+
 }
